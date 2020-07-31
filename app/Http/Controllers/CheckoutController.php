7eb3f6ccd1e3151705c8d1373d913cfb4fe40f19
IRFA <irfardy2@gmail.com>
2020-07-31 23:38:21 +0700

    checkout status

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\TemplateApp;
+use App\Models\Transaction;
+use App\Models\TransactionDetails;
 use Auth;
+use Validator;
+use Str;
+use Ramsey\Uuid\Uuid;
 
 
 class CheckoutController extends Controller
@@ -26,5 +31,60 @@ class CheckoutController extends Controller
 			$calc += $total;
 		}
     	return view('checkout.index')->with(['cart' => $cart,'total' => $calc]);
+    }
+    public function test(){
+    	$uid = Uuid::uuid4();;
+    	echo $uid->toString();
+    }
+    public function progress(Request $request){
+		$validator = Validator::make($request->all(),[
+                           'bank' =>'required|exists:bank,id',
+                       ]);
+           if ($validator->fails()){
+                $error= $validator->errors()->first();
+                 return redirect()->back()->with(['message_fail' => $error])
+                                          ->withInput($request->all());
+            }
+        $cart = Cart::where('user_id',Auth::user()->id)->get();
+		$calc =0;
+		foreach($cart as $s){
+			if(!empty($s->template->discount)){
+				$discount = $s->template->discount / 100;
+				$tanpa_diskon = $s->template->price;
+				$total = $tanpa_diskon-($s->template->price*$discount);
+			} else{
+				
+				$total = $s->template->price;
+			}
+			$calc += $total;
+		}
+		
+            $timeout = time()+(((3600)*24));//1 hari
+         	$generate_inv = "INV/".date("Y")."/".date("m")."/".strtoupper(Str::random(4));
+         	$params = [	'invoice' => $generate_inv,
+			         	'id_user' => Auth::user()->id,
+			         	'link' => Uuid::uuid4(),
+			         	'total_harga' => $calc,
+			         	'metode_pembayaran' => "TRANSFER",
+			         	'bank_id' => $request->bank,
+			         	'is_verify' => false,
+			         	'timeout' =>  $timeout,
+			         	'is_canceled' => false,
+			         	'updated_by' => Auth::user()->id];
+         $trans = Transaction::create($params);
+
+        $move = Cart::where('user_id',Auth::user()->id)->get();
+		foreach($move as $mv){
+			TransactionDetails::create(['transaksi_id' => $trans->id,'template_id' => $mv->template_id,'harga' => $mv->template->price - ($mv->template->price*($mv->template->discount/100)),'discount' => $mv->template->discount]);
+			Cart::where('id',$mv->id)->delete();
+		}
+		return redirect()->route('checkout.success',['id' => $trans->link])->with(['message_success' => "Mohon segera lakukan konfirmasi pembayaran"]);
+
+    }
+
+    public function success($id){
+    	$data = Transaction::where(['link' => $id,'id_user' => Auth::user()->id])->first();
+    	 
+    	return view('checkout.success')->with(['data' => $data]);
     }
 }
