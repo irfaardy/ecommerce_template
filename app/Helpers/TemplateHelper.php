c3f733570c4dcba294ec98111601fbbd34ec8246
IRFA <irfardy2@gmail.com>
2020-07-31 23:35:58 +0700

    Update Category

--- app/Helpers/TemplateHelper.php
+++ app/Helpers/TemplateHelper.php
@@ -3,6 +3,7 @@ namespace App\Helpers;
 use App\Models\CategoryTheme;
 use App\Models\Platform;
 use App\Models\Cart;
+use App\Models\Bank;
 use Auth;
 
 class TemplateHelper {
@@ -12,7 +13,7 @@ class TemplateHelper {
 
 			return $cat;
 		}
-		$cat = CategoryTheme::all();
+		$cat = CategoryTheme::where('hidden_from_category',false)->get();
 
 		return $cat;
 	}
@@ -20,6 +21,11 @@ class TemplateHelper {
 		$plt = Platform::all();
 
 		return $plt;
+	}
+	public static function bank(){
+		$bnk = Bank::orderBy('nama_bank','ASC')->get();
+
+		return $bnk;
 	}
 	public static function cart(){
 		$count = Cart::where(["user_id" => Auth::user()->id])->count();
