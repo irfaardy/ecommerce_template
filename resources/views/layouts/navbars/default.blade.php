9d087f532f371d57a27eeafdf3468e130da51b7a
IRFA <irfardy2@gmail.com>
2020-08-01 13:12:32 +0700

    add link home

--- resources/views/layouts/navbars/default.blade.php
+++ resources/views/layouts/navbars/default.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
         <nav class="navbar navbar-expand-lg  py-2 active">
             <div class="container">
-                <a href="#" class="navbar-brand text-uppercase font-weight-bold"><img src="{{asset('img/logo_wt.png')}}" height="50px" class="brand"></a>
+                <a href="{{route("LandingPage")}}" class="navbar-brand text-uppercase font-weight-bold"><img src="{{asset('img/logo_wt.png')}}" height="50px" class="brand"></a>
                 <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
 
                 <div class="collapse navbar-collapse in" id="navbarSupportedContent">
--- resources/views/layouts/navbars/landingpage.blade.php
+++ resources/views/layouts/navbars/landingpage.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
         <nav class="navbar_landing navbar navbar-expand-lg fixed-top py-2">
             <div class="container">
-                <a href="#" class="navbar-brand text-uppercase font-weight-bold"><img src="{{asset('img/logo_wt.png')}}" height="50px" class="brand"></a>
+                <a href="{{route("LandingPage")}}" class="navbar-brand text-uppercase font-weight-bold"><img src="{{asset('img/logo_wt.png')}}" height="50px" class="brand"></a>
                 <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
 
                 <div class="collapse navbar-collapse in" id="navbarSupportedContent">
